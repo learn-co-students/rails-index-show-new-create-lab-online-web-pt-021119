1c863467d1318cfa761d4e79e230004a0e283d2f
cyantis <cyantis@gmail.com>
2019-06-25 09:52:54 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources :coupons, only: [:index, :new, :create]
-  get '/show/:id', 'coupons#show', as: 'coupon'
+  resources :coupons, only: [:index, :new, :create, :show]
 end
